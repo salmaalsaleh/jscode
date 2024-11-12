6bcd1fa9d214b534a0cc73977abf0f6d9cb123e0
salmaalsaleh <salehsalma06@gmail.com>
2024-11-12 11:56:46 -0500

    Create scriptOne.js

---
+++ scriptOne.js
@@ -0,0 +1,45 @@
+/* 
+var age = 20;
+age = 100;
+console.log(age);
+
+
+let rollNo = 21;
+rollNo = 101;
+console.log(rollNo);
+
+
+const valuePi = 3.14;
+console.log(valuePi); 
+
+
+let age = 20;
+console.log(age);
+
+
+let userName = "Harry";
+console.log(userName);
+
+let pet = true;
+console.log(pet);
+
+let house = null;
+console.log(house);
+
+let bigNumber = 129023812934803489341n;
+console.log(bigNumber);
+*/
+
+let firstName = "Salma";
+let lastName = "Saleh";
+console.log(firstName , lastName)
+/*
+let a = 100;
+let b = 20;
+console.log(a * b);
+*/
+let a = 100;
+let b = 20;
+a = a + 10;
+a **= 10; a= a ** 10;
+console.log(a !=b);
