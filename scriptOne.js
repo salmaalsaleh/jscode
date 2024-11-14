abacc131261dd10acb65a00770e46356df4f5603
salmaalsaleh <salehsalma06@gmail.com>
2024-11-14 11:18:16 -0500

    Update scriptOne.js

--- scriptOne.js
+++ scriptOne.js
@@ -28,18 +28,43 @@ console.log(house);
 
 let bigNumber = 129023812934803489341n;
 console.log(bigNumber);
-*/
+
 
 let firstName = "Salma";
 let lastName = "Saleh";
 console.log(firstName , lastName)
-/*
+
 let a = 100;
 let b = 20;
 console.log(a * b);
-*/
+
 let a = 100;
 let b = 20;
 a = a + 10;
 a **= 10; a= a ** 10;
 console.log(a !=b);
+*/
+
+let house;
+let number = 100;
+if (number > 90){
+    house = "Grade A";
+    console.log(house);
+}
+
+else if(number > 80){
+    house = 'Grade B';
+    console.log(house);
+}
+
+else if(number > 70){
+    house = 'Grade C';
+    console.log(house);
+}
+
+else{
+    house = "fail"
+    console.log(house);
+}
+
+number > 90 ? "Grade A": "Fail";
